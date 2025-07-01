b28b2cf98604230a1e0191649e46eee0a5114968
Elizabeth Nelson <enelson@mchenry.edu>
2025-07-01 11:28:38 -0500

    Add files via upload

---
+++ grid/change.js
@@ -0,0 +1,24 @@
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
+
+
+
+/* change the image */
+function change01() {
+    document.getElementById("large").src = "images/city-dawn.jpg"; 
+}
+
+function change02() {
+    document.getElementById("large").src = "images/cliffs.jpg"; 
+}
+
+function change03() {
+    document.getElementById("large").src = "images/pied-flycatcher-ps.jpg"; 
+}
+
+function change04() {
+    document.getElementById("large").src = "images/swan-ps.jpg"; 
+}
+
+function change05() {
+    document.getElementById("large").src = "images/mountains-sunrise.jpg"; 
+}
