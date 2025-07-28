7fae3cff131e79da9ac4eeb92a986ccf3fb195ff
Elizabeth Nelson <enelson@mchenry.edu>
2025-07-28 16:38:44 -0500

    Add files via upload

---
+++ change.js
@@ -0,0 +1,20 @@
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
+
+
+
+/* change the image */
+function change01() {
+    document.getElementById("large").src = "images/beach-umbrellas.jpg"; 
+}
+
+function change02() {
+    document.getElementById("large").src = "images/15th-street-beach.jpg"; 
+}
+
+function change03() {
+    document.getElementById("large").src = "images/waves.jpg"; 
+}
+
+function change04() {
+    document.getElementById("large").src = "images/book-on-the-beach.jpg"; 
+}
